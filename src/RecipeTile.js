4fbc48a40bf71fe0c717e1fbca2b414f55b98d69
works-nikhil <works.nikhil@gmail.com>
2022-03-16 04:18:23 +0530

    changes

--- src/RecipeTile.js
+++ src/RecipeTile.js
@@ -7,7 +7,9 @@ export default function RecipeTile({ recipe }) {
     recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null   // to check if the object getting fetched is in image format only
     &&
     (
-      <div className='recipeTile'>
+      <div className='recipeTile' onClick={() => {
+        window.open(recipe["recipe"]["url"])
+      }}>
 
         <img recipe="recipe.id" alt="" className="recipeTile__img"
           src={recipe["recipe"]["image"]}
